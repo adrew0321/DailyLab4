86d721edff73fd14bc3c6cc184ffcd4b496e32c2
A'Keem Drew <adrew0321@gmail.com>
2018-07-19 09:41:54 -0400

    First commit

---
+++ LabNumber4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LabNumber4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //This programs displays a table of Powers
+
+            Console.WriteLine($"Learn your squares and cubes!");
+            Console.ReadLine();
+
+            Console.Write($"Enter an integer: ");
+            int userInput = int.Parse(Console.ReadLine());
+            Console.ReadLine();
+
+            String header = $"{"Number",-10} {"Squared",-11} {"Cubed",-10}";
+            String divider = $"{"_______",-10} {"_______",-10} {"_______",-10}";
+
+            Console.WriteLine($"{header}\n{divider}");
+
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                Console.WriteLine($"{i,-10} {i * i,-10} {i * i * i,-10}");
+            }
+
+        }
+
+
+
+    }
+}
